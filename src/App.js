88e7441a182642d8151dda72958cf0f879d29662
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2022-11-25 01:59:41 +0800

    added style

--- src/App.js
+++ src/App.js
@@ -4,13 +4,51 @@ function App() {
 
   const [count, setCount] = useState(0);
 
-
+  const forClick = {
+    'cursor': 'pointer',
+    
+  }
+  console.log(count)
 
   return (
    <>
-      <button onClick={ () => setCount(count + 1) }>+</button>
-       <h1>{ count }</h1>
-      <button onClick={ () => setCount(count - 1) }>-</button>
+   <div style={
+    {
+      'width': '100%',
+      'height': '100%',
+     
+      'margin': 'auto',
+      'padding': '10px',
+      'position': 'relative'
+      
+    }
+   }>
+      <div onClick={ () => setCount(count - 1) } style={ {
+        'width': '50%',
+        'height': '100%',
+        'background': 'red',
+        'float': 'left',
+        'cursor': 'pointer',
+        'color': "white",
+        "textAlign":'center',
+        'fontSize': '300px'
+       
+      } }>{count}</div>
+       {/* <h1 style={
+        {
+          
+        }
+       }>{ count }</h1> */}
+      <div onClick={ () => setCount(count + 1) } style={ {
+        'margin-left': '50%',
+        'height': '100%',
+        'background': 'black',
+        'cursor': 'pointer',
+        'color': 'white',
+        'textAlign':'center',
+        'fontSize': '300px'
+      } } >{count}</div>
+      </div>
    </>
   );
 }
